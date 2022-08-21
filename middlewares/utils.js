fac6f3bf31640dbfaa740cfce7d6e7b0eb8a8d12
sthefano <spolitino@popapp.io>
2022-08-21 23:54:33 +0200

    :

--- middlewares/utils.js
+++ middlewares/utils.js
@@ -2,9 +2,8 @@ const { request } = require("express");
 
 /**middleware personalizado para loguear cada vez que entra una request */
 const loguearRequest = (req = request, res, next) => {
-  console.log(req.path);
   console.log(
-    `Ingresa request al path ${JSON.stringify(req.route?.path)} desde IP ${
+    `Ingresa request al path ${JSON.stringify(req.originalUrl)} desde IP ${
       req.ip
     } con headers ${JSON.stringify(req.headers)}, tipo: ${
       req.method
